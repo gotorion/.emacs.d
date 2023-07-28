e6731bfcbf5548fc29f98083c7e1c4aeb08155c3
Junhui Li <junhui.lee@icloud.com>
2023-07-28 14:14:37 +0800

    add init.el

---
+++ init.el
@@ -0,0 +1,6 @@
+;;; init.el --- Load the full configuration
+;;; Code:
+(electric-pair-mode t) ; auto complete bracket
+(tool-bar-mode -1)     ; disable tool bar
+(global-display-line-numbers-mode 1) ; show line number
+;;; init.el ends here

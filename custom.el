9029b4d93b6c8fe1fde89a096f49f9fb9d2d7eb5
Junhui Li <junhui.lee@icloud.com>
2023-07-29 13:36:55 +0800

    add plugins

--- custom.el
+++ custom.el
@@ -3,7 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dracula-theme use-package)))
+ '(custom-safe-themes
+   '("467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" default))
+ '(delete-selection-mode nil)
+ '(package-selected-packages
+   '(gruvbox-theme doom-modeline-now-playing magit doom-themes doom-modeline vterm flycheck company helm vertico-prescient vertico keycast dracula-theme use-package))
+ '(warning-suppress-types '((comp) (comp) (comp) (comp) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,18 +1,71 @@
 ;;; init.el --- Load the full configuration
+;;; Commentary:
 ;;; Code:
 
 (require `package)
-(setq package-archives `(("gnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu")
+(setq package-archives `(("gnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
 			 ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
+(unless (package-installed-p `use-package)
+  ;; update local cache
+  (package-refresh-contents)
+  (package-install `use-package))
 (eval-when-compile
   (require `use-package))
+(setq user-package-always-ensure t)
 
-(load-theme `dracula t)
+;; install theme
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-gruvbox") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; modeline
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+;; magit
+(use-package magit)
+
+(use-package helm
+  :bind (("M-x" . helm-M-x)
+	 ("C-x C-f" . helm-find-files))
+  :config
+  (helm-mode 1))
+
+(use-package company
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-tooltip-align-annotations t
+	company-tooltip-limit 20
+	company-show-numbers t
+	company-idle-delay .2
+	company-minimum-prefix-length 1))
+(use-package flycheck
+  :init
+  (setq flycheck-emacs-lisp-load-path `inherit)
+  :config
+  (global-flycheck-mode))
+
+
 
 (add-to-list `load-path "~/.emacs.d/lisp")
-(require `init-basic)
+(require `basic)
 
 (provide `init)
 

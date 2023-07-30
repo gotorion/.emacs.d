8ead056193a2220452dfb922b9cf5488a6be8fd2
Junhui Li <junhui.lee@icloud.com>
2023-07-30 17:32:42 +0800

    add lsp

--- custom.el
+++ custom.el
@@ -3,11 +3,46 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#fafafa" "#99324b" "#4f894c" "#9a7500" "#3b6ea8" "#97365b" "#398eac" "#2a2a2a"])
+ '(custom-enabled-themes '(doom-xcode))
  '(custom-safe-themes
-   '("467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" default))
+   '("adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" default))
  '(delete-selection-mode nil)
+ '(exwm-floating-border-color "#c4c4c4")
+ '(fci-rule-color "#9e9e9e")
+ '(highlight-tail-colors ((("#e8eee8") . 0) (("#e6eff2") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#fafafa" "#3b6ea8"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#fafafa" "#4f894c"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#fafafa" "#bdbdbd"))
+ '(objed-cursor-color "#99324b")
  '(package-selected-packages
-   '(gruvbox-theme doom-modeline-now-playing magit doom-themes doom-modeline vterm flycheck company helm vertico-prescient vertico keycast dracula-theme use-package))
+   '(lsp-ui lsp-mode gruvbox-theme doom-modeline-now-playing magit doom-themes doom-modeline vterm flycheck company helm vertico-prescient vertico keycast dracula-theme use-package))
+ '(pdf-view-midnight-colors (cons "#2a2a2a" "#fafafa"))
+ '(rustic-ansi-faces
+   ["#fafafa" "#99324b" "#4f894c" "#9a7500" "#3b6ea8" "#97365b" "#398eac" "#2a2a2a"])
+ '(vc-annotate-background "#fafafa")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#4f894c")
+    (cons 40 "#688232")
+    (cons 60 "#817b19")
+    (cons 80 "#9a7500")
+    (cons 100 "#a0640c")
+    (cons 120 "#a65419")
+    (cons 140 "#ac4426")
+    (cons 160 "#a53f37")
+    (cons 180 "#9e3a49")
+    (cons 200 "#97365b")
+    (cons 220 "#973455")
+    (cons 240 "#983350")
+    (cons 260 "#99324b")
+    (cons 280 "#a25467")
+    (cons 300 "#ab7784")
+    (cons 320 "#b49aa0")
+    (cons 340 "#9e9e9e")
+    (cons 360 "#9e9e9e")))
+ '(vc-annotate-very-old-color nil)
  '(warning-suppress-types '((comp) (comp) (comp) (comp) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- Load the full configuration
 ;;; Commentary:
 ;;; Code:
-
+(setq url-proxy-services `(("http" . "127.0.0.1:7890")))
 (require `package)
 (setq package-archives `(("gnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
 			 ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
@@ -22,18 +22,47 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-xcode t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-gruvbox") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-xcode") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
+;; lsp-mode
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l"
+	lsp-auto-configure t
+	lsp-auto-guess-root t
+	lsp-idle-delay 0.500
+  )
+  :hook (
+	 ((c++-mode
+	   c-mode
+	   ) . lsp)
+	 (lsp-mode . lsp-enable-which-key-integration)
+	 )
+  :commands lsp)
+(use-package lsp-ui
+  :after (lsp-mode)
+  :commands (lsp-ui-mode)
+  :bind
+  (:map lsp-ui-mode-map
+	([remap xref-find-references] . lsp-ui-peek-find-references)
+	([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+	("C-c u" . lsp-ui-imenu))
+  :hook (lsp-mode . lsp-ui-mode)
+  :init
+  (setq lsp-enable-symbol-highlighting t
+	lsp-ui-doc-enable t
+	lsp-lens-enable t)
+  )
 ;; modeline
 (use-package doom-modeline
   :ensure t

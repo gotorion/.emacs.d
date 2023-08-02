95e09900ebf1d11c5aafa0e2d833154c5208b3a0
Junhui Li <junhui.lee@icloud.com>
2023-08-02 13:31:17 +0800

    disable menu bar and backup

--- lisp/basic.el
+++ lisp/basic.el
@@ -2,8 +2,13 @@
 ;;; Code:
 (electric-pair-mode t) ; auto complete bracket
 (add-hook 'prog-mode-hook #'show-paren-mode)
+(menu-bar-mode -1)
 (tool-bar-mode -1)     ; disable tool bar
 (global-display-line-numbers-mode 1) ; show line number
+
+;; disable backup and auto save
+(setq make-backup-files nil)
+
 
 (provide `basic)
 
